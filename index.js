3f596f4176cb7028f141cf7d1c8f40323dacecb8
Winny-code <canchaywiphuth@gmail.com>
2024-09-08 17:58:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,368 @@
+var produt = [ {
+    id: 1,
+    img: "./imgCord/Messenger_creation_bd97bfac-828f-4ce7-8e23-d70b352b9ed8.png",
+    name:"C",
+    type:"C"
+},{
+    id: 2,
+    img: "./imgCord/Messenger_creation_49141851-b067-4bb3-9936-f83b41c9178d.png",
+    name:"Cm",
+    type:"C"
+},{
+    id: 3,
+    img: "./imgCord/Messenger_creation_85d41165-665f-4b33-a936-1c41710a1885.png",
+    name:"Cmaj7",
+    type:"C"
+},{
+    id: 4,
+    img: "./imgCord/Messenger_creation_9dea2e9a-b81e-4eb5-b1de-fa592db89f2c.png",
+    name:"Cm7",
+    type:"C"
+},{
+    id: 5,
+    img: "./imgCord/Messenger_creation_c10bd39d-7f8f-4333-8d2a-5fedac09f5c5.png",
+    name:"C7",
+    type:"C"
+},{
+    id: 6,
+    img: "./imgCord/Messenger_creation_29345eea-fcd6-4eb9-993f-2270026119a2.png",
+    name:"C#",
+    type:"Db"
+},{
+    id: 7,
+    img: "./imgCord/Messenger_creation_c4df91aa-4fdf-452f-815b-cbe5c1848202.png",
+    name:"C#m",
+    type:"Db"
+},{
+    id: 8,
+    img: "./imgCord/Messenger_creation_6196294c-7c18-4057-92ed-b85c4df9bec7.png",
+    name:"C#maj7",
+    type:"Db"
+},{
+    id: 9,
+    img: "./imgCord/Messenger_creation_2ff5538a-a165-4ca6-9a37-c9be52b51351.png",
+    name:"C#m7",
+    type:"Db"
+},{
+    id: 10,
+    img: "./imgCord/Messenger_creation_9e84b409-13c3-42e1-bd05-3339ba9a26fe.png",
+    name:"C#7",
+    type:"Db"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_409f3308-ed08-4ca0-82a6-a4118fce9879.png",
+    name:"D",
+    type:"D"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_b2490b8e-2fde-4a27-aaad-91cdeb1df43c.png",
+    name:"Dm",
+    type:"D"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_8e7243bd-2a03-4c81-a324-6cca9593e04f.png",
+    name:"Dmaj7",
+    type:"D"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_e06efb90-406c-4e0f-b8ca-bf45a9b67e7b.png",
+    name:"Dm7",
+    type:"D"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_fc818913-130c-4425-8830-f2bcd976e80c.png",
+    name:"D7",
+    type:"D"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_037aada3-e22d-4608-a6f0-892ea946424e.png",
+    name:"D#",
+    type:"Eb"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_2581cdb4-9dbd-42ba-84c4-4c2b442b40e5.png",
+    name:"D#m",
+    type:"Eb"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_54a54c13-aabf-47f5-a75c-02f3970e4782.png",
+    name:"D#maj7",
+    type:"Eb"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_e83783a4-5d0b-4d36-9536-3f7563ce3f2d.png",
+    name:"D#m7",
+    type:"Eb"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_e12575f8-e7d0-479c-9dc6-05b727611251.png",
+    name:"D#7",
+    type:"Eb"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_9372a9b6-aa50-49ed-87fb-6cd2beaa3a78.png",
+    name:"E",
+    type:"E"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_829cb29a-0f49-4185-ae83-52f932321912.png",
+    name:"Em",
+    type:"E"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_e7a972c8-8fa7-4c24-98ef-ed7dc03aa893.png",
+    name:"Emaj7",
+    type:"E"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_e2ab9ef9-cdc5-4a23-99dd-694605ecf62d.png",
+    name:"Em7",
+    type:"E"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_677e7a2d-12c7-4a70-8d59-5a4838ade9a7.png",
+    name:"E7",
+    type:"E"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_bee11ff1-30e0-448f-b56a-695c2d2e3116.png",
+    name:"F",
+    type:"F"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_1d88fb5b-8db2-40d2-a772-14ad23839405.png",
+    name:"Fm",
+    type:"F"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_bd3bfe42-0ea8-403d-b885-95ac2324472b.png",
+    name:"Fmaj7",
+    type:"F"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_96ef3f64-ba23-45c0-bc71-9111d92c5c91.png",
+    name:"Fm7",
+    type:"F"
+},{
+    id: 1,
+    img: "./imgCord/Messenger_creation_604879ad-7d24-4f08-8a06-fd330cb465a5.png",
+    name:"F7",
+    type:"F"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_b5fd18d7-1d66-4121-ad7f-f952d739a343.png",
+    name:"F#",
+    type:"Gb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_35df4279-e42b-425b-8fa6-d67803faad7f.png",
+    name:"F#m",
+    type:"Gb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_ae3cad63-7da3-4776-a9bf-79c6f812236c.png",
+    name:"F#maj7",
+    type:"Gb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_b0194da8-a3d2-4454-97bd-0b9d94f46c2d.png",
+    name:"F#m7",
+    type:"Gb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_c2f8f4bf-e0b6-4006-9f4d-cd509360e801.png",
+    name:"F#7",
+    type:"Gb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_0669b001-b5a7-41cf-8e8a-ec37e93e9b02.png",
+    name:"G",
+    type:"G"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_23f4a138-202c-45af-a9d6-f9e538c17f79.png",
+    name:"Gm",
+    type:"G"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_5a8622b6-194b-4f2e-bf2b-0bc729c061b0.png",
+    name:"Gmaj7",
+    type:"G"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_2d331ece-2449-4435-b3bc-f00b64c4c5c4.png",
+    name:"Gm7",
+    type:"G"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_b4592df7-dc8e-4cf7-a20c-f8af195693ab.png",
+    name:"G7",
+    type:"G"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_2785a71c-5037-45ee-8cd3-95173ef2b782.png",
+    name:"G#",
+    type:"Ab"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_c6ee1582-3f15-4e47-8641-da18a8e773f9.png",
+    name:"G#m",
+    type:"Ab"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_07e170e7-250e-4ec4-acf2-63aadd9a2914.png",
+    name:"G#maj7",
+    type:"Ab"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_959356a3-bf82-4e74-9b74-97685e242abc.png",
+    name:"G#m7",
+    type:"Ab"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_72dbe26b-9281-4911-8cbd-ffd287a4b7bb.png",
+    name:"G#7",
+    type:"Ab"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_587b8597-ff85-46de-8d36-9022250d446e.png",
+    name:"A",
+    type:"A"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_a9c2dc4e-3d04-4050-81c4-5fcfd2bfca56.png",
+    name:"Am",
+    type:"A"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_8999c0ce-1b01-4bfc-9c27-52e47450a538.png",
+    name:"Amaj7",
+    type:"A"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_37c1b585-0a7f-41e1-aaeb-c7567886044d.png",
+    name:"Am7",
+    type:"A"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_d900a9f0-9468-4cf4-a9af-a5a2a9223017.png",
+    name:"A7",
+    type:"A"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_9443f380-e056-44c2-913a-005f4e80fd34.png",
+    name:"A#",
+    type:"Bb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_1de4b793-5545-4362-8e7d-ec677a1a909b.png",
+    name:"A#m",
+    type:"Bb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_06018b2b-f444-4bb9-b8be-7fa53e1a89cb.png",
+    name:"A#maj7",
+    type:"Bb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_78551dc0-ab17-4124-b507-06dea5cd70f6.png",
+    name:"A#m7",
+    type:"Bb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_710de6b7-e32d-4d17-b992-da016acf8942.png",
+    name:"A#7",
+    type:"Bb"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_bd8e1698-99f4-4eda-94bf-2629298e4ed1.png",
+    name:"B",
+    type:"B"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_ac4f23c6-abb3-4219-99c7-fe29b808f23d.png",
+    name:"Bm",
+    type:"B"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_488e0a0a-296b-4333-802b-74e962772675.png",
+    name:"Bmaj7",
+    type:"B"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_59fbee94-ff41-425f-aaef-f5127f684661.png",
+    name:"Bm7",
+    type:"B"
+},{
+    id: 1,
+    img: "./imgCord2/Messenger_creation_70b33920-b9f7-4d24-8d92-bf6bf84bb1a5.png",
+    name:"B7",
+    type:"B"
+}];
+
+$(document).ready(() => { 
+    var html = '';
+    for (let i = 0; i < produt.length; i++) {
+        html += `  <div onclick="opencorddetail(${i})" class="product-items ${produt[i].type}">
+                <img class ="product-img" src="${produt[i].img}" alt="">
+                <p style="font-size: 2vw;">${produt[i].name}</p>
+            </div>`;
+    
+    }
+    $("#productlist").html(html);
+
+})
+
+
+function searchsomething(elem) {
+    console.log(elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < produt.length; i++) {
+        if( produt[i].name.includes(value) ) {
+        html += `  <div onclick="opencorddetail(${i})" class="product-items ${produt[i].type}">
+                <img class ="product-img" src="${produt[i].img}" alt="">
+                <p style="font-size: 2vw;">${produt[i].name}</p>
+            </div>`;
+        }
+    }
+    if(html == ''){
+        $("#productlist").html(`<p> หาคอร์ดไม่เจอ </p>`);
+
+    } else{
+        
+        $("#productlist").html(html);
+
+    }
+
+    
+
+}
+
+function searchproduct(Cord){
+    console.log(Cord)
+    $(".product-items").css('display','none')
+    if(Cord == 'All' ){
+        $(".product-items").css('display','block')
+    }
+    else{
+        $('.'+Cord).css('display','block')
+    }
+
+}
+
+
+var cordindex = 0;
+function opencorddetail(index) {
+    cordindex = index;
+    console.log(cordindex)
+    $("#modalcord").css('display','flex')
+    $("#mdd-img").attr('src',produt[index].img);
+
+}
+
+function closeModal() {
+    $(".modal").css("display","none")
+}
